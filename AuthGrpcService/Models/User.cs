fad5a75e41aba3a76635c472cf49eec552bd63dd
1 <teykasoc@gmail.com>
2026-05-28 19:34:18 +0200

    feat: add EF Core with PostgreSQL and AppDbContext

---
+++ AuthGrpcService/Models/User.cs
@@ -0,0 +1,8 @@
+namespace AuthGrpcService.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Login { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+}
