2b8a8383f3595b2d9bf87985424d34be17989fb0
1 <teykasoc@gmail.com>
2026-05-31 18:52:13 +0200

    chore: move JWT secrets to env vars and expose app port in compose

--- AuthGrpcService/Program.cs
+++ AuthGrpcService/Program.cs
@@ -1,18 +1,50 @@
+using System.Text;
 using AuthGrpcService.Data;
+using AuthGrpcService.Models;
 using AuthGrpcService.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add services to the container.
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+builder.Services.AddScoped<JwtTokenService>();
+
 builder.Services.AddGrpc();
+
+var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>()!;
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = jwtSettings.Issuer,
+            ValidateAudience = true,
+            ValidAudience = jwtSettings.Audience,
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key)),
+            ValidateIssuerSigningKey = true,
+        };
+    });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapGrpcService<AuthService>();
 app.MapGet("/",
     () =>
